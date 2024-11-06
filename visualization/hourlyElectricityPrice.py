cffcb7bd0153db26eae05f272fdc2c9c43be73f9
corqi <maciek.kowalik12@gmail.com>
2024-11-06 23:09:35 +0100

    Added visualization for hourly price of electricity

---
+++ visualization/hourlyElectricityPrice.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+file_path = '../spotpriser.csv'
+df = pd.read_csv(file_path)
+
+df['hour'] = df['Dato/klokkeslett'].str.extract(r'Kl\. (\d{2})').astype(int)
+
+average_prices_no2 = df.groupby('hour')['NO2'].mean()
+average_prices_no2.loc[24] = average_prices_no2.loc[23]
+
+plt.figure(figsize=(10, 6))
+plt.step(average_prices_no2.index, average_prices_no2.values, where='post', color='#81C784', label='Average Price')
+plt.title('Average Electricity Price per Hour')
+plt.ylabel('Average Price (NOK/kWh)')
+plt.xlabel('Hour of Day')
+
+plt.xticks(range(25))
+plt.xlim(0,24)
+plt.grid(True)
+plt.legend()
+plt.tight_layout()
+plt.show()
