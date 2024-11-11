e9b0707b4dffb1c0d92f255c74c347446c869ee3
Harald <haraldwangsvik@gmail.com>
2024-11-11 09:52:26 +0100

    correct house number to color

--- visualization/peakPrices.py
+++ visualization/peakPrices.py
@@ -29,15 +29,27 @@ print(m_peaks_2)
 print(m_peaks_3)
 print(m_peaks_4)
 
+total_consumption = {
+    'Building 10724 (solar)': df_solar['Verdi'].sum(),
+    'Building 4746': df_2['Verdi'].sum(),
+    'Building 4462': df_3['Verdi'].sum(),
+    'Building 10703': df_4['Verdi'].sum()
+}
+
+# Print the total consumption for each building
+print("Total Consumption for Each Building (KW):")
+for building, consumption in total_consumption.items():
+    print(f"{building}: {consumption:.2f} KW")
+
 # Plot the montly KW maxes
 plt.figure(figsize=(10, 6))  # Set figure size
 plt.title('Monthly peak energy consumption (KW)')
 plt.ylabel('KW')
 plt.xlabel('Month')
 plt.plot(m_peaks_solar.index, m_peaks_solar.values, marker='o', color='#66b3ff', label='Building 10724 (solar)')
-plt.plot(m_peaks_2.index, m_peaks_2.values, marker='o', color='#ffcc99', label='Building 4746')
+plt.plot(m_peaks_2.index, m_peaks_2.values, marker='o', color='#99ff99', label='Building 10703')
 plt.plot(m_peaks_3.index, m_peaks_3.values, marker='o', color='#ff9999', label='Building 4462')
-plt.plot(m_peaks_4.index, m_peaks_4.values, marker='o', color='#99ff99', label='Building 10703')
+plt.plot(m_peaks_4.index, m_peaks_4.values, marker='o', color='#ffcc99', label='Building 4746')
 
 # Add grid and legend
 plt.grid(True)
